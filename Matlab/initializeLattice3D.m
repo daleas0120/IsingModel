c095adac6c4f1df8b9379d7c274ec4e958d77593
daleas202 <62962861+daleas202@users.noreply.github.com>
2020-05-09 09:15:54 -0400

    pass arg for boundary condition
    Correctly initialize boundary conditions based on passed argument

--- Matlab/initializeLattice3D.m
+++ Matlab/initializeLattice3D.m
@@ -1,12 +1,17 @@
-function spinD = initializeLattice3D(N,D)
+function spinD = initializeLattice3D(N,D,b)
 %{
 %initializeLattice3D.m
 %Ashley Dale
 %creates a randomly initialized NxN lattice
 
+N: number of edge spins
+D: number of stacked layers
+b: [1, 0 -1]; lock edge spins into HS, open boundary, or LS
+
 %}
 
-spinD = ones(N);
+spinD = b.*ones(N);
+
 for kdx = 1:(D-2)
     spins = rand(N-2); %decide how many ones there are
     for idx = 1:(N-2)
@@ -18,8 +23,11 @@ for kdx = 1:(D-2)
             end
         end
     end
-    spins = padarray(spins,[1 1],1,'both');
+    spins = padarray(spins,[1 1],b,'both');
     spinD = cat(3,spinD, spins);
 end
-    spinD = cat(3, spinD, ones(N));
+
+spinD = cat(3, spinD, b.*ones(N));
+
 end
+
