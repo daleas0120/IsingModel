adb850e95815a962f9e5fd5daee02a2462b632a2
daleas202 <62962861+daleas202@users.noreply.github.com>
2021-04-06 12:41:42 -0400

    Update n_HSfrac.m

--- Matlab/n_HSfrac.m
+++ Matlab/n_HSfrac.m
@@ -7,14 +7,14 @@ Calculates the high-spin fraction for a 2D matrix of spins with the border
 spins locked in high spin
 %}
 
-N = max(size(spins));
+[N, M] = size(spins);
 
-sum_Si = sum(spins(2:N-1, 2:N-1), 'all');
+sum_Si = sum(spins(2:N-1, 2:M-1), 'all');
 %sum_Si = sum(spins, 'all');
 
 %mean = ((4*(N - 1)) + sum_Si)/(N*N);
 
-mean = sum_Si/((N-2)^2);
+mean = sum_Si/((N-2)*(M-2));
 
 nHS = (1+mean)/2;
 
